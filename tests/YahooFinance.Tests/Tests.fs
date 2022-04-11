6d0109805ec771bcf10fd5fff686ff7892d56a6d
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2022-04-11 11:22:24 +0100

    move test code into tests.fs (but still needs to be turned into actual tests).

--- tests/YahooFinance.Tests/Tests.fs
+++ tests/YahooFinance.Tests/Tests.fs
@@ -3,6 +3,30 @@ module Tests
 open System
 open Xunit
 
+open Price.Series
+
+let techstocks = ["MSFT"; "AMZN"; "IBM"; "AAPL"; "NovaSbeInc."]
+
+let response = 
+    techstocks
+    |> List.map BuildQuery.create
+    |> getSeries
+
+let msftSeries = 
+    response.Data
+    |> List.filter (fun xs -> xs.Meta.Symbol = "MSFT")
+    |> List.exactlyOne
+    |> fun msft -> msft.History
+
+let ibmDiv = 
+    response.Data
+    |> List.filter (fun xs -> xs.Meta.Symbol = "IBM")
+    |> List.exactlyOne
+    |> fun ibmSeries ->  ibmSeries.Events.Dividends
+
+let missing = response.ErrorLog
+
+
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
