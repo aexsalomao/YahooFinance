c8e0e4eeb21c7de5f44e483e0a42a371e680fd57
Antônio Salomão <83646729+aexsalomao@users.noreply.github.com>
2022-08-08 20:38:06 -0300

    Intro file
    For some reason this still doesn't work ... I get 2 different messages on my terminal ...
    
    1. It can't find the file
    2. The file i'm trying to access may be blocked by F# interactive
    
    Still looking into it ...

--- docs/intro.fsx
+++ docs/intro.fsx
@@ -4,16 +4,16 @@ open System
 open Quotes.YahooFinance
 // Why are these returning empty lists?
 
-let aapl = Series.History("AAPL",
-                          startDate = DateTime(1990,1,1), 
-                          endDate = DateTime.Now)
+let aapl = Series.Download("AAPL",
+                           startDate = DateTime(1990,1,1), 
+                           endDate = DateTime.Now)
 
 aapl
 
-let aapl2 = Series.History(["AAPL"],
+let aapl2 = Series.Download(["AAPL"],
                           startDate = DateTime(2020,1,1), 
                           endDate = DateTime.Now.AddDays(-1))
 aapl2
 
 // doesn't show any logs.
-Series.History("AAPL",displayLogs=true)
+Series.Download("AAPL")
