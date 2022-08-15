1cda408a13be1573471a9e9220351cc74a10746b
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2022-08-15 14:33:33 -0500

    working intro

--- docs/intro.fsx
+++ docs/intro.fsx
@@ -1,19 +1,15 @@
+
 #r "../src/Quotes.YahooFinance/bin/Debug/net6.0/Quotes.YahooFinance.dll"
 
+// If you don't reference FSharp.Data it will fail silently!
+#r "nuget: FSharp.Data"
 open System
 open Quotes.YahooFinance
-// Why are these returning empty lists?
 
-let aapl = Series.Download("AAPL",
+let aapl = YahooFinance.History("AAPL",
                            startDate = DateTime(1990,1,1), 
                            endDate = DateTime.Now)
 
-aapl
+["MSFT"; "AMZN"; "IBM"; "AAPL"; "NovaSbeInc."]
+|> YahooFinance.History 
 
-let aapl2 = Series.Download(["AAPL"],
-                          startDate = DateTime(2020,1,1), 
-                          endDate = DateTime.Now.AddDays(-1))
-aapl2
-
-// doesn't show any logs.
-Series.Download("AAPL")
