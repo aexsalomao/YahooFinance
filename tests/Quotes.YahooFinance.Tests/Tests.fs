911dbc47014a9ab6d3c2c6beeb06e51e056f5b61
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2022-08-22 13:42:37 -0500

    open/high/low/close are split adjusted. Using floats now.

--- tests/Quotes.YahooFinance.Tests/Tests.fs
+++ tests/Quotes.YahooFinance.Tests/Tests.fs
@@ -11,4 +11,4 @@ open Quotes.YahooFinance
 let ``Series.Download returns ChartSeries list`` () =
     ["MSFT"; "AMZN"; "IBM"; "AAPL"; "NovaSbeInc."]
     |> YahooFinance.History 
-    |> should be ofExactType<list<Quote>> 
+    |> should be ofExactType<list<Quote>>    
